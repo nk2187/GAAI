9c449d1cea3b758ff9ad6a018c4ced793d8a7f6a
RK <kridha.5k@gmail.com>
2025-10-28 03:05:26 +0000

    also option to select month and year should be there

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -23,7 +23,9 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
+        caption_dropdowns: "flex gap-2",
+        vhidden: "hidden",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -61,6 +63,9 @@ function Calendar({
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
+      captionLayout="dropdown-buttons"
+      fromYear={new Date().getFullYear() - 10}
+      toYear={new Date().getFullYear()}
       {...props}
     />
   )
